be1512353f0df4db02a5f86f21bf60de9e4664b5
kimmisung <kayaa65@naver.com>
2025-07-23 00:06:35 +0900

    java master

---
+++ src/jvm/LottoApplication.java
@@ -0,0 +1,23 @@
+package jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class LottoApplication {
+    private static final List<Set<Integer>> lottoHistory = new ArrayList<>();
+
+    public static void main(String[] args) {
+        LottoGenerator generator = new LottoGenerator();
+        while (true) {
+            Set<Integer> lottoNumbers = generator.generate();
+            System.out.println(lottoNumbers);
+
+            if (lottoHistory.size() > 100) {
+                lottoHistory.clear();
+            }
+
+            lottoHistory.add((Set<Integer>) lottoNumbers);
+        }
+    }
+}
---
+++ src/jvm/LottoGenerator.java
@@ -0,0 +1,15 @@
+package jvm;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class LottoGenerator {
+    public Set<Integer> generate() {
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < 6) {
+            numbers.add(ThreadLocalRandom.current().nextInt(1,46));
+        }
+        return numbers;
+    }
+}
