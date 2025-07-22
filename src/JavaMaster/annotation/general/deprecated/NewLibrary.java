18b408b8c4f67d08531818832fbf443648a056e8
kimmisung <kayaa65@naver.com>
2025-07-23 00:07:33 +0900

    [Annotation, Reflection] General Annotation: Override

---
+++ src/JavaMaster/annotation/general/deprecated/NewLibrary.java
@@ -0,0 +1,7 @@
+package JavaMaster.annotation.general.deprecated;
+
+public class NewLibrary {
+    public void printMessage() {
+        System.out.println("I'm new library");
+    }
+}
---
+++ src/JavaMaster/annotation/general/deprecated/OldLibrary.java
@@ -0,0 +1,9 @@
+package JavaMaster.annotation.general.deprecated;
+
+public class OldLibrary {
+    @Deprecated
+    public void printMessage() {
+        System.out.println("I'm old library");
+    }
+}
+
